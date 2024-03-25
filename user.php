53f77feb7ac020c47309ee9403ab7f4e88124b33
rozo <d.zwirski336@wp.pl>
2024-03-25 13:18:30 +0100

    zrobiona rejestracja

--- user.php
+++ user.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_REQUEST['email']) && isset( $_REQUEST['password'])){
+if (isset($_REQUEST['action']) && $_REQUEST['action'] == "login" ){
 //$password = "tajnehasło";
 //$hash = password_hash($password, PASSWORD_ARGON2I);
 //echo $hash;
@@ -26,6 +26,30 @@ if($userRow == null ){
          echo"błąd w logowaniu debilu <br>";
     }
 }
+
+if (isset($_REQUEST['action']) && $_REQUEST['action'] == "login" )
+{
+$bd = new mysqli("localhost" , "root", "", "auth");
+$email = $_REQUEST['email'];
+$email = filter_var($email , FILTER_SANITIZE_EMAIL);
+$password = $_REQUEST['password'];
+$passwordReapeat = $_REQUEST['passwordRepeat'];
+
+if($password == $passwordReapeatd );
+}
+$q = $db->prepare("INSERT INTO user VALUES (NULL,? , ? )");
+$passwordHash = password_hash($password, PASSWORD_ARGON2I); 
+$q->bind_praram("ss , $email , $passwordHash");
+$result = $q->execute();
+if($result){
+    echo "Konto utworzone popawnie";
+
+}else{
+    echo "coś poszło nie tak";
+}
+
+}else{
+    echo "hasła nie są zgodne - spróbuj ponownie";
 }
 
 
@@ -35,21 +59,24 @@ if($userRow == null ){
 ?>
 
 <form action="user.php"method="post">
-<label for="emailimput">Email:</label>
-<input type="email" name="email" id="emailimput">
-<label for="passwordinput">Hasło; </label>
-<input type="password" name="password" id="passwordimput">
+<label for="emailInput">Email:</label>
+<input type="email" name="email" id="emailInput">
+<label for="passwordInput">Hasło; </label>
+<input type="password" name="password" id="passwordInput">
+<input type="hidden" name="action" value="login">
 <input type="submit" value="Zaloguj">
 
 </form>
 
 <h1>Zarajestruj się </h1>
 <form action="user.php"method="post">
-<form action="user.php"method="post">
 <label for="emailimput">Email:</label>
 <input type="email" name="email" id="emailimput">
-<label for="passwordRepeat">Hasło; </label>
-<input type="password" name="password" id="passwordRepeat">
-<input type="submit" value="Zaloguj">
+<label for="passwordInput">Hasło; </label>
+<input type="password" name="password" id="passwordInput">
+<label for="passwordRepeatInput">Hasło Ponownie</label>
+<input type="password" name="passwordRepeat" id="passwordRepeatInput">
+<input type="hidden" name="action" value="register">
+<input type="submit" value="Zarejestruj">
    46 minuta
 </form>
